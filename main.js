e036acf6dd7322aba96dc770dda5e31bbc2f63a1
Lenrui <132875948+Lenrul@users.noreply.github.com>
2023-06-07 05:36:24 -0700

    main

---
+++ main.js
@@ -0,0 +1,7 @@
+function customyt(){
+    var ifrm = document.createElement("IFRAME"); 
+    ifrm.setAttribute("src", "https://www.youtube.com/watch?v=-QhAf7nfHVs"); 
+    ifrm.style.width = "50%"; 
+    ifrm.style.height = "50%"; 
+    document.body.appendChild(ifrm); 
+  } 
